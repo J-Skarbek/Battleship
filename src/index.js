0f5cf452cf7a66dd3920d9905d8dff4a3f682215
J-Skarbek <john@snarkmediaagency.com>
2023-01-08 14:41:34 -0600

    Add npm watch and inline source maps.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
 import './style.css';
 import TestImg from './sun copy.jpg';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
@@ -13,6 +15,11 @@ function component() {
    myIcon.src = TestImg;
  
    element.appendChild(myIcon);
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
