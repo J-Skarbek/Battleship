ccbdf21bdeef4a96cac68ea70b041ca80ab5c2c5
J-Skarbek <john@snarkmediaagency.com>
2023-01-09 18:41:40 -0600

    Start with rough-draft of shipfactory function.

---
+++ src/ship.js
@@ -0,0 +1,14 @@
+const shipFactory = (length, name) => {
+  const isSunk = false;
+  const numberofHits = 0;
+  const statusReport = (length) => {
+    if (numberOfHits = length) {
+      isSunk = true;
+    }
+  };
+  return {
+    length,
+    name,
+    isSunk,
+  };
+};
